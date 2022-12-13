25a36e671e88db3962b72c5ab65b108bd8fd4ebb
Decole mills <okpoka92@gmail.com>
2022-12-13 16:45:55 +0100

    add last

--- app/component/SignInComponent.tsx
+++ app/component/SignInComponent.tsx
@@ -10,12 +10,13 @@ function SignInComponent({providers}: Props) {
        Object.values(providers!).map((provider) => (
             <div key={provider.id}>
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-2 rounded" onClick={() => signIn(provider.id, {
-                    callbackUrl: https://process.env.VERCEL_URL || "http://localhost:3000"
+                    callbackUrl: `https://process.env.VERCEL_URL` || "http://localhost:3000"
                 })}>
                  Sign in with {provider.name}
                 </button>
                 </div>
-        ))}
+                ))
+        }
     </div>
     
   
